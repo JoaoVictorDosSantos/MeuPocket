6f1288f53c6a85b741a9b01a9965acfe2668610d
santos.joao@aluno.ifsp.edu.br <joao_97victorsantos@hotmail.com>
2020-08-26 22:05:35 -0300

    Implementação - Parte 3

---
+++ app/src/main/java/br/edu/dmos5/meupocket/view/RecyclerItemClickListener.java
@@ -0,0 +1,7 @@
+package br.edu.dmos5.meupocket.view;
+
+/*** Essa interface é que permitirá o tratamento do clique*
+ nos itens do RecyclerView*/
+public interface RecyclerItemClickListener {
+    void onItemClick(int position);
+}
